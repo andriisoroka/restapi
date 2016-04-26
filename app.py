ad60c108d5916596b5cae6c5ba9dbc6cf882a202
Andrey Soroka <andrii_soroka@ukr.net>
2016-04-26 13:28:44 +0300

    Test!

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from app import app
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
